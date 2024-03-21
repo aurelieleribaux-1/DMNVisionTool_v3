e64768919c316d72bff3944eb269623d01d4ec62
aurelieleribaux-1 <aurelie.leribaux@student.kuleuven.be>
2024-03-21 10:15:16 +0100

    Update HomeComponent.vue

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -14,42 +14,110 @@
       <!-- Image Upload Section -->
       <section class="upload-section">
         <h2>Get Started</h2>
-        <q-btn
-          color="positive"
-          icon="upload_file"
-          :label="$t('home.upload_image')"
-          @click="filePicker?.pickFiles()"
-        ></q-btn>
-        <q-file
-          ref="filePicker"
-          style="display: none"
-          accept=".png, .jpeg, .jpg, .bmp"
-          v-model="file"
-          @update:model-value="loadImage(file as File)"
-        ></q-file>
+        <div style="display: flex; align-items: center;">
+          <!-- First upload button -->
+          <q-btn
+            color="positive"
+            icon="upload_file"
+            :label="$t('home.upload_image')"
+            @click="filePicker?.pickFiles()"
+          ></q-btn>
+          <q-file
+            ref="filePicker"
+            style="display: none"
+            accept=".png, .jpeg, .jpg, .bmp"
+            v-model="file"
+            @update:model-value="loadImage(file as File)"
+          ></q-file>
+
+          <!-- Second upload button -->
+          <q-btn
+            color="positive"
+            icon="upload_file"
+            :label="$t('home.upload_image')"
+            @click="filePicker2?.pickFiles()"
+          ></q-btn>
+          <q-file
+            ref="filePicker2"
+            style="display: none"
+            accept=".png, .jpeg, .jpg, .bmp"
+            v-model="file2"
+            @update:model-value="loadImage2(file2 as File)"
+          ></q-file>
+        </div>
       </section>
 
       <!-- Options Section -->
       <section class="options-section">
         <h2>Customize Your Experience</h2>
         <div class="options">
-          <q-checkbox
-            class="q-pr-md"
-            v-model="elementsEnabled"
-            :label="$t('home.show_elements')"
-          ></q-checkbox>
-          <q-checkbox
-            :disable="!elementsEnabled"
-            class="q-pr-md"
-            v-model="flowsEnabled"
-            :label="$t('home.show_flows')"
-          ></q-checkbox>
-          <q-checkbox
-            :disable="!elementsEnabled"
-            class="q-pr-md"
-            v-model="ocrEnabled"
-            label="Enable OCR"
-          ></q-checkbox>
+          <!-- Checkboxes for first image -->
+          <div>
+            <q-checkbox
+              class="q-pr-md"
+              v-model="elementsEnabled"
+              :label="$t('home.show_elements')"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled"
+              class="q-pr-md"
+              v-model="flowsEnabled"
+              :label="$t('home.show_flows')"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled"
+              class="q-pr-md"
+              v-model="ocrEnabled"
+              label="Enable OCR"
+            ></q-checkbox>
+            <!-- Additional checkboxes for specifying image type -->
+            <q-checkbox
+              :disable="!elementsEnabled"
+              class="q-pr-md"
+              v-model="isGraph"
+              label="Graph"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled"
+              class="q-pr-md"
+              v-model="isTable"
+              label="Table"
+            ></q-checkbox>
+          </div>
+
+          <!-- Checkboxes for second image -->
+          <div>
+            <q-checkbox
+              class="q-pr-md"
+              v-model="elementsEnabled2"
+              :label="$t('home.show_elements')"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled2"
+              class="q-pr-md"
+              v-model="flowsEnabled2"
+              :label="$t('home.show_flows')"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled2"
+              class="q-pr-md"
+              v-model="ocrEnabled2"
+              label="Enable OCR"
+            ></q-checkbox>
+            <!-- Additional checkboxes for specifying image type -->
+            <q-checkbox
+              :disable="!elementsEnabled2"
+              class="q-pr-md"
+              v-model="isGraph2"
+              label="Graph"
+            ></q-checkbox>
+            <q-checkbox
+              :disable="!elementsEnabled2"
+              class="q-pr-md"
+              v-model="isTable2"
+              label="Table"
+            ></q-checkbox>
+          </div>
         </div>
         <q-btn
           :disable="!imageLoaded"
@@ -62,24 +130,47 @@
 
       <!-- Image Display Section -->
       <section class="image-display">
-        <h2>Your Uploaded Image</h2>
-        <div class="image-container">
-          <q-img
-            :style="'border: 1px ' + ($q.dark.mode ? 'gray' : 'black') + ' solid'"
-            sizes="(max-width: 400px) 400px, (max-height: 400px) 400px"
-            fit="contain"
-            position="50% 50%"
-            width="400px"
-            height="400px"
-            placeholder-src="../assets/default-placeholder.png"
-            no-spinner
-            :src="(imgSrc as string)"
-            @load="loadingOK"
-            @error="loadingError"
-            @dragover="allowDrop($event)"
-            @drop="drop($event)"
-          >
-          </q-img>
+        <h2>Your Uploaded Images</h2>
+        <div class="image-container" style="display: flex;">
+          <!-- First image display -->
+          <div style="margin-right: 20px;">
+            <q-img
+              v-if="imgSrc"
+              :style="'border: 1px ' + ($q.dark.mode ? 'gray' : 'black') + ' solid'"
+              sizes="(max-width: 200px) 200px, (max-height: 200px) 200px"
+              fit="contain"
+              position="50% 50%"
+              width="200px"
+              height="200px"
+              placeholder-src="../assets/default-placeholder.png"
+              no-spinner
+              :src="imgSrc"
+              @load="loadingOK"
+              @error="loadingError"
+              @dragover="allowDrop($event)"
+              @drop="drop($event)"
+            ></q-img>
+          </div>
+
+          <!-- Second image display -->
+          <div>
+            <q-img
+              v-if="imgSrc2"
+              :style="'border: 1px ' + ($q.dark.mode ? 'gray' : 'black') + ' solid'"
+              sizes="(max-width: 200px) 200px, (max-height: 200px) 200px"
+              fit="contain"
+              position="50% 50%"
+              width="200px"
+              height="200px"
+              placeholder-src="../assets/default-placeholder.png"
+              no-spinner
+              :src="imgSrc2"
+              @load="loadingOK"
+              @error="loadingError"
+              @dragover="allowDrop($event)"
+              @drop="drop($event)"
+            ></q-img>
+          </div>
         </div>
       </section>
     </main>
