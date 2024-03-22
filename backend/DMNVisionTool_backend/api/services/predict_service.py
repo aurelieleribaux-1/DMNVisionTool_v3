147ed204a503491cd98b3addd6d85e2f75d36101
aurelieleribaux-1 <aurelie.leribaux@student.kuleuven.be>
2024-03-22 14:03:39 +0100

    Update predict_service.py

--- backend/DMNVisionTool_backend/api/services/predict_service.py
+++ backend/DMNVisionTool_backend/api/services/predict_service.py
@@ -25,7 +25,7 @@ class ObjectPredictor:
         cfg.merge_from_file(
             model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
         )
-        cfg.OUTPUT_DIR = here("../../detectron_model/Output") # Make sure to use right directory
+        cfg.OUTPUT_DIR = here("../../detectron_model") # Make sure to use right directory
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "DRD_model_final.pth")  # path to the trained model
         #cfg.MODEL.WEIGHTS = here("../../detectron_model/final_model.pth") # Make sure to use the right name
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
@@ -68,7 +68,7 @@ class KeyPointPredictor:
         cfg.merge_from_file(
             model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_1x.yaml")
         )
-        cfg.OUTPUT_DIR = here("../../detectron_model/OutputFlowV3")
+        cfg.OUTPUT_DIR = here("../../detectron_model")
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "kp_DRD_model_final.pth")
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
@@ -174,7 +174,7 @@ class TablePredictor:
         cfg.merge_from_file(
             model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
         )
-        cfg.OUTPUT_DIR = here("../../detectron_model/OutputTable") # Make sure to use right directory
+        cfg.OUTPUT_DIR = here("../../detectron_model") # Make sure to use right directory
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "Table_model_final.pth")  # path to the trained model
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 26
@@ -217,7 +217,7 @@ class TableElementPredictor:
         cfg.merge_from_file(
             model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
         )
-        cfg.OUTPUT_DIR = here("../../detectron_model/OutputTableElement") # Make sure to use right directory
+        cfg.OUTPUT_DIR = here("../../detectron_model") # Make sure to use right directory
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "TableStructure_model_final.pth")  # path to the trained model
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 26
