7e0e5eea3b6d45f2aea2bd0b95275f9368c5986f
aurelieleribaux-1 <154355950+aurelieleribaux-1@users.noreply.github.com>
2024-05-07 15:37:23 +0200

    Update sketch_ocr_service.py

--- backend/DMNVisionTool_backend/api/services/sketch_ocr_service.py
+++ backend/DMNVisionTool_backend/api/services/sketch_ocr_service.py
@@ -14,22 +14,23 @@ import numpy as np
 import cv2
 import nltk
 from collections import Counter
+import Levenshtein
 
 # Load English words dictionary from nltk
 nltk.download('words')
 english_words = set(nltk.corpus.words.words())
 
 def get_valid_word(word, all_words):
-    if word.lower() in english_words:
-        return word
+    english_word_matches = []
+    for w in all_words:
+        if w.lower() in english_words:
+            english_word_matches.append(w)
+    if english_word_matches:
+        best_match = min(english_word_matches, key=lambda x: Levenshtein.distance(word.lower(), x.lower()))
+        return best_match
     else:
-        # If the word is not in the dictionary, return a word from the list of all words detected in the image
-        for w in all_words:
-            if w.lower() in english_words:
-                return w
-        # If no valid replacement word is found, return a placeholder word or handle it as desired
-        return "word"
-
+        # If no valid replacement word is found, correct the existing word 
+        return TextBlob(word).correct().string
 
 def get_text_from_img(img,predictions: List[ObjectPrediction]):
     """Extract text from an image using OCR with Tesseract after ROI selection.
@@ -63,7 +64,7 @@ def get_text_from_img(img,predictions: List[ObjectPrediction]):
         # Resize the ROI based on the resizing factor
         roi_resized = cv2.resize(roi, None, fx=resize_factor, fy=resize_factor, interpolation=cv2.INTER_CUBIC)
         # Perform OCR on the ROI
-        text = pytesseract.image_to_string(roi_resized, config="--psm 12 --oem 1")
+        text = pytesseract.image_to_string(roi_resized, config="--psm 12 --oem 1 -c tessedit_char_blacklist={+-,./?;:@[]()*&^%$£!¬`}|><_}")
         # Tokenize the text
         words = text.split()
         # Initialize a set to keep track of unique words in the corrected text
@@ -149,7 +150,69 @@ def get_text_from_img2(img, elements: List[Element]):
             
     return elements
 
-def get_text_from_table_img_sketch(img, table_elements: List[TableElement]) -> List[TableElement]:
+def get_text_from_table_img_sketch(img,predictions: List[TablePrediction]):
+    """Extract text from an image using OCR with Tesseract after ROI selection.
+    Parameters
+    ----------
+    img: ndarray
+        The image to use for text extraction (as Numpy ndarray)
+
+    Returns
+    -------
+    List[GraphText]
+        The list of detected text with bounding boxes
+    """
+    # Convert the image to grayscale
+    #gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if len(predictions) > 19:
+        resize_factor = 2
+    else: 
+        resize_factor = 1
+    # Initialize list to store extracted text with bounding boxes
+    text_list = []
+
+    # Iterate over the predictions and extract text from each predicted box
+    for prediction in predictions:
+        x1, y1, x2, y2 = prediction.get_box_coordinates()
+        # Extract the ROI from the grayscale image
+        roi = img[int(y1):int(y2), int(x1):int(x2)]
+        # Resize the ROI based on the resizing factor
+        roi_resized = cv2.resize(roi, None, fx=resize_factor, fy=resize_factor, interpolation=cv2.INTER_CUBIC)
+        # Perform OCR on the ROI
+        if len(predictions) > 19:
+          # Perform OCR on the ROI
+          text = pytesseract.image_to_string(roi_resized, config="--psm 6 --oem 1 -c tessedit_char_blacklist={+-}")
+        else: 
+          text = pytesseract.image_to_string(roi_resized, config="--psm 12 --oem 1 -c tessedit_char_blacklist={+-}")
+        # Tokenize the text
+        words = text.split()
+        # Initialize a set to keep track of unique words in the corrected text
+        unique_words = set()
+        # Replace nonsensical words with the most frequent English word
+        corrected_text = []
+        for word in words:
+            if word.lower() not in english_words:
+                # If the word is not in the English words dictionary, replace it with a valid word
+                valid_word = get_valid_word(word, words)
+                if valid_word not in unique_words:
+                    # Add the valid word to the corrected text and the set of unique words
+                    corrected_text.append(valid_word)
+                    unique_words.add(valid_word)
+            else:
+                # If the word is in the English words dictionary, keep it in the corrected text
+                corrected_text.append(word)
+        # Join the corrected text into a single string
+        corrected_text = " ".join(corrected_text)
+        # Create a GraphText object with the corrected text and bounding box
+        table_text = GraphText(corrected_text, x1, y1, x2 - x1, y2 - y1)
+        # Append the GraphText object to the list
+        text_list.append(table_text) 
+
+    return text_list
+
+
+def get_text_from_table_img_sketch2(img, table_elements: List[TableElement]) -> List[TableElement]:
     """Extract all the text from an image using OCR with pytesseract
 
     Parameters
@@ -225,4 +288,25 @@ def link_text(texts: List[GraphText], elements: List[Element]):
     for text in texts:
         nearest = get_nearest_element(text.center, elements)
         nearest.name.append(text)
+    return elements
+
+# for tables: !!! 2 classes called Text !!!
+def link_text_table(texts: List[TableText], elements: List[TableElement]):
+    """Method that links the Text to the corresponding Elements
+
+    Parameters
+    ----------
+    texts: List[TableText]
+        List of detected Text
+    elements: List[TableElement]
+        List of Element to be linked
+
+    Returns
+    -------
+    List[TableElement]
+        The list of updated Element
+    """
+    for text in texts:
+        nearest = get_nearest_element(text.center, elements)
+        nearest.label.append(text)
     return elements
